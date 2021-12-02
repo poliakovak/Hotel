d98a73e1b246b425e2162b642b4283cedab1e3bf
poliakovak <kseniiapoliakova01@gmail.com>
2021-12-02 11:56:20 +0200

    Added Main class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import controllers.GuestController;
+import models.Guest;
+import views.GuestView;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Guest model = new Guest();
+        GuestView view = new GuestView(model);
+        GuestController controller = new GuestController(model, view);
+        controller.runApp();
+    }
 }
