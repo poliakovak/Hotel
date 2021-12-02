a1df85683e16f6923a388b218152321dd38380e1
poliakovak <kseniiapoliakova01@gmail.com>
2021-12-02 09:52:58 +0200

    Add getters and setters to GuestModel

--- src/models/GuestModel.java
+++ src/models/GuestModel.java
@@ -1,9 +1,22 @@
 package models;
 
+
 public class GuestModel {
 
     private String name;
     private int nightsNumber;
     private int guestsNumber;
     private int starsNumber;
+
+    public String getName() {return name; }
+    public void setName(String name) {this.name = name; }
+
+    public int getNightsNumber() {return nightsNumber; }
+    public void setNightsNumber(int nightsNumber) {this.nightsNumber = nightsNumber; }
+
+    public int getGuestsNumber() {return guestsNumber; }
+    public void setGuestsNumber(int guestsNumber) {this.guestsNumber = guestsNumber; }
+
+    public int getStarsNumber() {return starsNumber; }
+    public void setStarsNumber(int starsNumber) {this.starsNumber = starsNumber;}
 }
