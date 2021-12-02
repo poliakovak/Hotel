23fd64eead1206fb269a9d9e534f7b3a7c085791
poliakovak <kseniiapoliakova01@gmail.com>
2021-12-02 12:46:54 +0200

    Added method for calculating price with kids

--- src/base/Payment.java
+++ src/base/Payment.java
@@ -3,4 +3,6 @@ package base;
 public interface Payment {
 
     double calculatePayment(double price, int guestNumber, int nightsNumber);
+
+    double calculatePayment(double price, int guestNumber, int kidsNumber, int nightsNumber);
 }
